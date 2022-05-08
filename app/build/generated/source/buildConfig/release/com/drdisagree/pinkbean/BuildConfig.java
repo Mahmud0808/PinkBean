33b855d90ede58eda598134c9eb2d264fae93124
DrDisagree <crazymahmud08@gmail.com>
2022-05-08 19:34:38 +0600

    Initial build

---
+++ app/build/generated/source/buildConfig/release/com/drdisagree/pinkbean/BuildConfig.java
@@ -0,0 +1,22 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.drdisagree.pinkbean;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.drdisagree.pinkbean";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: release
+  public static final String APK_SIGNATURE_PRODUCTION = "";
+  public static final String BASE_64_LICENSE_KEY = "";
+  public static final boolean ENFORCE_GOOGLE_PLAY_INSTALL = false;
+  // Fields from default config.
+  public static final boolean ALLOW_THIRD_PARTY_SUBSTRATUM_BUILDS = true;
+  public static final byte[] DECRYPTION_KEY = {97, 86, -9, 28, -80, 98, 121, -114, -125, 53, 72, 108, 113, 106, 19, 106};
+  public static final boolean ENABLE_APP_BLACKLIST_CHECK = true;
+  public static final byte[] IV_KEY = {79, 8, 43, -61, 60, 46, -89, 44, 98, -75, -119, -54, -103, 3, -42, 109};
+  public static final boolean SUPPORTS_THIRD_PARTY_SYSTEMS = true;
+}
